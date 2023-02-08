c830f2e6f5c35874b496d8512e9da5ea45f60c8f
Jgg023 <mightyfresh1@gmail.com>
2023-02-08 11:05:08 -0500

    text

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,12 +6,8 @@ export default function About() {
       <h1>Julian  Graves </h1> 
       <img src={process.env.PUBLIC_URL + authorPic} height="300" width="300" className=" float-left myPic" alt="Julian Graves" />
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        Hello , my name is Julian lets start with the science experiments ! With react you can factor in mulitiple component including UI/UX giving your page the ultimate styling with scripts including .sass .jsx and html respectfully . 
+      
       </p>
     </div>
   );
